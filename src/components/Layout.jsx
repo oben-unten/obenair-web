a2a22e3360334c357076dd585e55c8f07ee43d30
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-12-07 22:30:38 +0100

    Add header elements

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => <div>
+const Layout = ({ children }) => <>
+    <Helmet>
+        <title>Oben Air Festival</title>
+        <meta name="description" content="Grenzübergreifendes Musik- und Kunstfestival bei Zell an der Mosel."></meta>
+    </Helmet>
     <Navbar />
     { children }
-</div>;
+</>;
 
 export default Layout;
