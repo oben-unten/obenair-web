d17606c626332d6506da9408c65aa40e9c6bac19
Sebastian Cuy <sebastian.cuy@dainst.de>
2022-03-17 12:55:31 +0100

    Fix styles in cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,8 @@
 import CMS from 'netlify-cms-app';
 import StartseitePreview from '../previews/StartseitePreview';
 import VereinPreview from '../previews/VereinPreview';
-import styles from '../styles/global.css';
 
 CMS.registerPreviewTemplate('Startseite', StartseitePreview);
 CMS.registerPreviewTemplate('Verein', VereinPreview);
 
-CMS.registerPreviewStyle(styles.toString(), { raw: true });
+CMS.registerPreviewStyle('../styles/global.css', { raw: true });
