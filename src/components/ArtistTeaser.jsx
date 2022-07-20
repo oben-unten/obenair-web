9297b1ed2bdc923129be1a1dab8229a58aa00c99
Sebastian Cuy <sebastian.cuy@dainst.de>
2022-07-20 13:51:55 +0200

    Update ArtistTeaser.jsx

--- src/components/ArtistTeaser.jsx
+++ src/components/ArtistTeaser.jsx
@@ -7,7 +7,7 @@ const X_POSITIONS = ['right-4', 'right-1/4', 'right-2/4', 'left-4', 'left-1/4', 
 const Y_POSITIONS = ['top-4', 'top-1/4', 'top-2/4', 'bottom-4', 'bottom-1/4', 'bottom-2/4'];
   
 const ArtistTeaser = ({ artist }) => <>
-        <div className="relative group mb-5">
+        <div className="relative group mb-5 break-inside-avoid">
             <Link to={`/artists/${artist.name}`}>
                 <Image image={artist.image} alt={`Foto von ${artist.title}`} className="md:grayscale hover:grayscale-0"/>
                 <h1 className={`text-3xl md:text-5xl text-black group-hover:text-white absolute ${generatePosition()}`}>{artist.title}</h1>
