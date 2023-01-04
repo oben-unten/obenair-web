a30cffcc872569297031de1a728066478dbcd232
Sebastian Cuy <sebastian.cuy@deepl.com>
2023-01-04 20:20:21 +0100

    Make text color compatible to dark image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = ({ transparent=false }) => {
         return () => document.removeEventListener('scroll', handleScroll);
     }, []);
 
-    const bgClass = (!transparent || scrolled || dropdownToggled) ? 'bg-header text-on-header' : 'bg-transparent';
-    const svgClass = (!transparent || scrolled || dropdownToggled) ? 'fill-on-header' : 'fill-on-light';
+    const bgClass = (!transparent || scrolled || dropdownToggled) ? 'bg-header text-on-header' : 'bg-transparent text-on-dark';
+    const svgClass = (!transparent || scrolled || dropdownToggled) ? 'fill-on-header' : 'fill-on-dark';
 
     return <>
         <nav className={`${bgClass} fixed w-full z-30 top-0 duration-300 ease-in-out`}>
