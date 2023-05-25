786c7ffb9c4cb2a7fb839036622d4da69e789929
Sebastian Cuy <sebastian.cuy@deepl.com>
2023-05-25 15:48:06 +0200

    Fix ticket iframe height

--- src/pages/tickets.jsx
+++ src/pages/tickets.jsx
@@ -8,9 +8,7 @@ const Tickets = () => {
         <div className="flex flex-col md:pt-20 pt-12 pb-5 md:mx-20">
             <iframe src="https://obenair.ticket.io/"
                 title="Tickets"
-                style={{width: "100%", height: "600px", border: "none2"}}
-                scrolling="auto"
-                frameBorder="0"
+                className="w-full h-screen"
                 allowtransparency="true">
             </iframe>
             <script async src="https://obenair.ticket.io/iframe.js"></script>
